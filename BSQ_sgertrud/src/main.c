3f6fdd6c28300943982e54243e4a39ebddb5a3fe
msnazarow <60715299+msnazarow@users.noreply.github.com>
2020-02-20 14:06:04 +0300

    sdf

---
+++ BSQ_sgertrud/src/main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshelton <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 21:41:30 by mshelton          #+#    #+#             */
+/*   Updated: 2020/02/19 21:41:32 by mshelton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+void	reading_string(int argc, char **argv, int f)
+{
+	int i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		f = open(argv[i], O_RDONLY);
+		if (f == -1)
+		{
+			ft_puterror(argv[i]);
+			ft_puterror(": map error\n");
+			if (i != argc - 1)
+				write(2, "\n", 1);
+		}
+		else if (make_tab(f))
+		{
+			ft_puterror(argv[i]);
+			ft_puterror(": map error\n");
+			if (i != argc - 1)
+				write(2, "\n", 1);
+		}
+		else if (i != argc - 1)
+			write(1, "\n", 1);
+		close(f);
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int		f;
+	int		i;
+	clock_t	t;
+
+	t = clock();
+	i = 0;
+	if ((f = 0) || argc == 1)
+		if (make_tab(f))
+			ft_puterror("map error\n");
+	reading_string(argc, argv, f);
+	return (0);
+}
